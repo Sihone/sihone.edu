9fb378c096a262d1c76271efa877cd8b80b62891
Austin Takam <austin.takam@sihone.com>
2023-12-05 21:16:16 -0500

    Verifying roles and permissions (#29)

--- client/src/app/views/forms/FormsRoutes.js
+++ client/src/app/views/forms/FormsRoutes.js
@@ -7,7 +7,6 @@ const WizardForm = Loadable(lazy(() => import("./WizardForm")));
 const OrderForm = Loadable(lazy(() => import("./order-form/OrderForm")));
 const InvoiceForm = Loadable(lazy(() => import("./invoice-form/InvoiceForm")));
 const PropertyListingForm = Loadable(lazy(() => import("./PropertyListingForm")));
-const CustomerForm = Loadable(lazy(() => import("../pages/customers/customer-form/CustomerForm")));
 
 const formsRoutes = [
   { path: "/forms/basic", element: <BasicForm /> },
@@ -15,7 +14,6 @@ const formsRoutes = [
   { path: "/forms/wizard", element: <WizardForm /> },
   { path: "/forms/order-form", element: <OrderForm /> },
   { path: "/forms/property-listing-form", element: <PropertyListingForm /> },
-  { path: "/forms/customer-form", element: <CustomerForm /> },
   { path: "/forms/invoice-form", element: <InvoiceForm /> },
 ];
 
